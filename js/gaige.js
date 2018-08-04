e9cc3496774d8c58abba4ea255b79bbde244653f
LGY <947696149@qq.com>
2018-08-05 00:34:46 +0800

    LGY

--- js/gaige.js
+++ js/gaige.js
@@ -15,48 +15,48 @@ $(document).ready(function() {
 		n++;
 		if(n%2!=0){
 			cho1.animate({left:'0'});
-			cho2.css("left","80%");
-			cho3.css("left","80%");
-			cho4.css("left","80%");
+			cho2.css("left","90%");
+			cho3.css("left","90%");
+			cho4.css("left","90%");
 		}
 		else{
-			cho1.css("left","80%");
+			cho1.css("left","90%");
 		}
 	});
 	snag2.click(function() {
 		n++;
 		if(n%2!=0){
 			cho2.animate({left:'0'});
-			cho1.css("left","80%");
-			cho3.css("left","80%");
-			cho4.css("left","80%");
+			cho1.css("left","90%");
+			cho3.css("left","90%");
+			cho4.css("left","90%");
 		}
 		else{
-			cho2.css("left","80%");
+			cho2.css("left","90%");
 		}
 	});
 	snag3.click(function() {
 		n++;
 		if(n%2!=0){
 			cho3.animate({left:'0'});
-			cho2.css("left","80%");
-			cho1.css("left","80%");
-			cho4.css("left","80%");
+			cho2.css("left","90%");
+			cho1.css("left","90%");
+			cho4.css("left","90%");
 		}
 		else{
-			cho3.css("left","80%");
+			cho3.css("left","90%");
 		}
 	});
 	snag4.click(function() {
 		n++;
 		if(n%2!=0){
 			cho4.animate({left:'0'});
-			cho2.css("left","80%");
-			cho1.css("left","80%");
-			cho3.css("left","80%");
+			cho2.css("left","90%");
+			cho1.css("left","90%");
+			cho3.css("left","90%");
 		}
 		else{
-			cho4.css("left","80%");
+			cho4.css("left","90%");
 		}
 	});
 //		for(n=1;n<=4;n++){
